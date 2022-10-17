678e52fda06ebf89e2dcdaec3c576d3e026d0931
kikuchi-syunsuke <kyunto.site@gmail.com>
2022-10-17 17:53:21 +0900

    material iconをインストールしました

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import SidebarOption from "./SidebarOption";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const Sidebar: React.FC = () => {
     return (
         <div className="sidebar">
+            <TwitterIcon />
             <SidebarOption text="ホーム" icon="アイコン" />
         </div>
-    )
-}
+    );
+};
 
 export default Sidebar;

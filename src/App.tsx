423f2f7d6209d2b7126363bcee0a7e07578107df
kikuchi-syunsuke <kyunto.site@gmail.com>
2022-10-18 15:28:40 +0900

    Sidebarコンポーネントにstyled-componentsを適用しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
+import styled from "styled-components";
 
 function App() {
-  return (
-    <div className="App">
-      <Sidebar />
-    </div>
-  );
+    return (
+        <SApp>
+            <Sidebar />
+        </SApp>
+    );
 }
+const SApp = styled.div`
+    display: flex;
+    height: 100vh;
+    max-width: 1300px;
+    margin: 0 auto;
+    padding: 0 10px;
+`;
 
 export default App;
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,11 +8,12 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import PermIdentityIcon from "@mui/icons-material/PermIdentity";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import styled from "styled-components";
 
 const Sidebar: React.FC = () => {
     return (
-        <div className="sidebar">
-            <TwitterIcon />
+        <SSidebar>
+            <SSidebarAppIcon />
             <SidebarOption text="ホーム" Icon={HomeIcon} />
             <SidebarOption text="話題を検索" Icon={SearchIcon} />
             <SidebarOption text="通知" Icon={NotificationsNoneIcon} />
@@ -21,9 +22,38 @@ const Sidebar: React.FC = () => {
             <SidebarOption text="リスト" Icon={ListAltIcon} />
             <SidebarOption text="プロフィール" Icon={PermIdentityIcon} />
             <SidebarOption text="もっと見る" Icon={MoreHorizIcon} />
-            <button>投稿する</button>
-        </div>
+            <SSidebarButton>投稿する</SSidebarButton>
+        </SSidebar>
     );
 };
+
+const SSidebar = styled.div`
+    border-right: 1px solid var(--app-color);
+    flex: 0.2;
+    min-width: 250px;
+    margin-top: 20px;
+    padding-inline: 20px;
+`;
+
+const SSidebarAppIcon = styled(TwitterIcon)`
+    color: var(--app-color);
+    font-size: 30px !important;
+    margin-left: 20px;
+    margin-bottom: 20px;
+`;
+
+const SSidebarButton = styled.button`
+    background-color: var(--app-color) !important;
+    color: white;
+    font-weight: 900 !important;
+    border: none !important;
+    border-radius: 30px !important;
+    height: 50px !important;
+    margin-top: 20px !important;
+    &:hover {
+        transition: 0.2s;
+        opacity: 0.5;
+    }
+`;
 
 export default Sidebar;

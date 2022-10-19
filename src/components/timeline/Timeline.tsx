1dc2d5c6557d4b924d633acb7e2f2829afaf9c9f
kikuchi-syunsuke <kyunto.site@gmail.com>
2022-10-19 11:16:09 +0900

    Postコンポーネントにスタイルをあて、Timelineコンポーネントからpropsを渡しました

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,4 +1,37 @@
+import Post from "./Post";
+import TweetBox from "./TweetBox";
+
+type POST = {
+    displayName: string;
+    username: string;
+    text: string;
+    avatar: string;
+    image: string;
+};
+
+const userPost: POST = {
+    displayName: "test-man",
+    username: "testtest",
+    text: "テスト投稿です",
+    avatar: "http://shincode.info/wp-content/uploads/2021/12/icon.png",
+    image: "https://source.unsplash.com/random",
+};
+
 const Timeline: React.FC = () => {
-    return <div>Timeline component</div>;
+    return (
+        <div>
+            <div>
+                <h2>ホーム</h2>
+            </div>
+            <TweetBox />
+            <Post
+                displayName={userPost.displayName}
+                username={userPost.username}
+                text={userPost.text}
+                avatar={userPost.avatar}
+                image={userPost.image}
+            />
+        </div>
+    );
 };
 export default Timeline;
